37e5d01c06a450889bc2285223dacdbe9e41921b
Kajal Pandit <kajalpandit830@gmail.com>
2026-04-02 20:55:57 +0530

    Added frontend url

--- FRONTEND/src/api/axios.js
+++ FRONTEND/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://lettercraftai-backend.onrender.com/"          //http://localhost:8080/api
+  baseURL: "https://lettercraftai-backend.onrender.com"          //http://localhost:8080/api
 });
 
 api.interceptors.request.use((config) => {
